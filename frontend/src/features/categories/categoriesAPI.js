9cacc03a0fc39ef1bb36b0a2167bea806ea2483a
Quonn S Bernard <quonn.bernard@gmail.com>
2023-04-04 14:00:23 -0500

    .

--- frontend/src/features/categories/categoriesAPI.js
+++ frontend/src/features/categories/categoriesAPI.js
@@ -4,6 +4,7 @@ const url = process.env.REACT_APP_API_CAT_URL;
 
 export const getCategories = async () => {
     console.log('2')
+    console.log(url)
     const response = await axios.get(url)
     return response.data
 }
